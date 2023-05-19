d7f3b808eb6e3cef027023b05b30878f044554b3
KanaiMint <k022g0015@g.neec.ac.jp>
2023-05-20 04:31:04 +0900

    playerの移動とかタイルマップとか
    うまくいかなかったー－－－－－－

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float MoveSpeed;//プレイヤーの動く速さ
+    [SerializeField] PlayerController player_;
+    [SerializeField] Transform camera_;　//カメラ
+    [SerializeField] float JumpForce = 350f;    //ジャンプの力
+    private bool jumpcount = false;
+    private Rigidbody2D rbody2D;
+    //private Collision2D collision2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //MoveSpeed = 0.0f;
+        rbody2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        //OnCollisionFloor(collision2D);
+        CameraUpdate();
+    }
+
+    void Move()
+    {
+
+        player_.transform.Rotate(0, 0, 0);
+        if (Input.GetKey(KeyCode.A))    //左に移動する処理    
+        {
+            player_.transform.position = new Vector3(transform.position.x - MoveSpeed, transform.position.y);
+        }
+        if (Input.GetKey(KeyCode.D))    //右に移動する処理
+        {
+            player_.transform.position = new Vector3(transform.position.x + MoveSpeed, transform.position.y);
+        }
+        if (Input.GetKey(KeyCode.W))    //上に移動（いらない）
+        {
+            player_.transform.position = new Vector3(transform.position.x , transform.position.y + MoveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))    //下に移動（いらない）
+        {
+            player_.transform.position = new Vector3(transform.position.x , transform.position.y - MoveSpeed);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && this.jumpcount==false)
+        
+        {
+            this.rbody2D.AddForce(transform.up * JumpForce);    //ジャンプの処理
+           // jumpcount=true;    //ジャンプ回数制限のためのカウント
+        }
+    }
+
+    private void OnCollisionFloor(Collision2D other)
+    {
+        //if (other.tag=="Floor")
+        //{
+        //    jumpcount = false;
+        //}
+    }
+
+    private void CameraUpdate()
+    {
+        camera_.transform.position = new Vector3(player_.transform.position.x,player_.transform.position.y,-10f);
+    }
+}
